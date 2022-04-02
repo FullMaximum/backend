0413b2c9e765b9c779524519f17f17b3f0b72f3d
Rokas <r.bagdonas2000@gmail.com>
2022-04-02 17:41:54 +0300

    (FEB-1) Setting up the solution and repository

---
+++ FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Controllers/BasicController.cs
@@ -0,0 +1,34 @@
+﻿using FlowersBEWebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowersBEWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {
+        private static List<UserBase> _users = new();
+        [HttpGet("getUser")]
+        public async Task<ActionResult<UserBase>> Get()
+        {
+            var user = new UserBase
+            {
+                Id = 1,
+                FirstName = "Rokas",
+                LastName = "Bagdonas",
+                Email = "x@gmail.com",
+            };
+            _users.Add(user);
+            return Ok(user);
+        }
+
+        [HttpPost("newUser")]
+        public async Task<ActionResult<List<UserBase>>> Create(UserBase user)
+        {
+            if (user == null)
+                return BadRequest("User is null");
+            _users.Add(user);
+            return Ok(_users);
+        }
+    }
+}
---
+++ FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Entities/UserBase.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowersBEWebApi.Entities
+{
+    public class UserBase
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
