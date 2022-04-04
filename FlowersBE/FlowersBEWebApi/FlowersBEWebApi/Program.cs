de90db124cd1ded25885d35d5f59069d6c1aed44
Rokas Bagdonas <rokas.bagdonas@tglab.onmicrosoft.com>
2022-04-04 19:39:36 +0300

    (FEB-9) setting dbcontext with simple injector

--- FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Program.cs
+++ FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Program.cs
@@ -10,28 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Dependency injection with SimpleInjector
 
-//var container = new Container();
-//container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+var container = new Container();
+container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
 
-//container.Register<DbContext, DataContext>(Lifestyle.Scoped);
-//container.Register<IBasicRepository, BasicRepository>(Lifestyle.Scoped);
-//container.Register<IBasicService, BasicService>(Lifestyle.Scoped);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+container.Register<DataContext>(() =>
+{
+    var options = new DbContextOptionsBuilder<DataContext>();
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    return new DataContext(options.Options);
+});
+container.Register<IBasicRepository, BasicRepository>(Lifestyle.Scoped);
+container.Register<IBasicService, BasicService>(Lifestyle.Scoped);
 
-//container.Verify();
-
-builder.Services.AddScoped<IBasicRepository, BasicRepository>();
-builder.Services.AddScoped<IBasicService, BasicService>();
+container.Verify();
 
 var app = builder.Build();
 
@@ -49,3 +47,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+//});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//builder.Services.AddScoped<IBasicRepository, BasicRepository>();
+//builder.Services.AddScoped<IBasicService, BasicService>();
