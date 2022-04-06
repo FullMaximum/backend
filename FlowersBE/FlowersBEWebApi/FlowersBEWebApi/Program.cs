ddcddfa9d5f60896ca82286627ec95fc4869e0b3
Dominykas <71326962+darkixem@users.noreply.github.com>
2022-04-06 20:56:51 +0300

    Sentry

--- FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Program.cs
+++ FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Program.cs
@@ -1,31 +1,40 @@
 global using FlowersBEWebApi.Data;
 global using Microsoft.EntityFrameworkCore;
 using FlowersBEWebApi;
+using Sentry;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//Dependency injection with SimpleInjector
-ObjectContainer.Init(builder);
-
-var app = builder.Build();
-
-ObjectContainer.VerifyApp(app);
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (SentrySdk.Init(o =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    o.Dsn = "https://c51dbab596cc4d09bdc25c910835aac9@o1187302.ingest.sentry.io/6307231";
+    o.Debug = true;
+    o.TracesSampleRate = 1.0;
+}))
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    //Dependency injection with SimpleInjector
+    ObjectContainer.Init(builder);
+
+    var app = builder.Build();
+
+    ObjectContainer.VerifyApp(app);
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
