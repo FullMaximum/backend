48f422c6b4869fd42c900726b766141669bc8ad6
Dominykas <71326962+darkixem@users.noreply.github.com>
2022-05-15 13:49:45 +0300

    shop entity

--- FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Entities/Shop.cs
+++ FlowersBE/FlowersBEWebApi/FlowersBEWebApi/Entities/Shop.cs
@@ -6,9 +6,15 @@ namespace FlowersBEWebApi.Entities
     {
         public long Id { get; set; }
         public string? Name { get; set; }
+        public string? Phone { get; set; }
         public string? ImagePath { get; set; }
         public string? Description { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? OpeningHour { get; set; }
+        public string? ClosingHour { get; set; }
         public float Rating { get; set; }
         public ShopStatus Status { get; set; }
     }
+
 }
